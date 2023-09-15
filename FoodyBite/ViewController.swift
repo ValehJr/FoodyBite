103b80e10442085eba946450aea0029fb80a6a30
Valeh Ismayilov <i.valehjr@gmail.com>
2023-09-15 20:13:14 +0400

    Initial Commit

---
+++ FoodyBite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodyBite
+//
+//  Created by Valeh Ismayilov on 15.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
